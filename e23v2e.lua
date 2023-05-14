a58ee64a01a3c185991cbdc617b4084e898954a4
dg2iugesgh <133449025+dg2iugesgh@users.noreply.github.com>
2023-05-14 17:23:59 +0300

    Update e23v2e.lua

--- e23v2e.lua
+++ e23v2e.lua
@@ -2,3 +2,11 @@ whitelist = {
   "yazagit_lera2013",
   "Inventarik2"
 }
+
+function Set(list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+
+whitelist = Set(whitelist)
